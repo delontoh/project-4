e5321ed1aa8d1c4c487182e5ecf26f6218cbd925
delontoh <delontoh89@gmail.com>
2018-08-16 20:25:41 +0800

    before google maps added

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,4 +8,8 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :content, :skill_1, :skill_2, :skill_3, :skill_4, :skill_5)
   end
+
+  def after_update_path_for(resource)
+    profile_user_path(current_user.id)
+  end
 end
