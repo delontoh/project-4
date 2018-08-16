39ab03f360695e08caf97af327593c33d5a31afe
delontoh <delontoh89@gmail.com>
2018-08-16 15:12:58 +0800

    styled user profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < ActionController::Base
     def after_sign_up_path_for(resource)
         profile_user_path(current_user.id)
     end
+
+    def after_update_path_for(resource)
+        profile_user_path(current_user.id)
+    end
 end
